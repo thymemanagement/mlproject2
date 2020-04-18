6cfab4de23dbf906d5a95460a74156bb14695817
thymemanagement <yhtomite@gmail.com>
2020-04-18 15:32:44 -0700

    refactored mini-project.py

--- mini-project.py
+++ mini-project.py
@@ -2,32 +2,25 @@ import numpy as np
 import csv
 import re
 
-parser = '\[\s*(\S*)\s+(\S*)\s*\]'
-
-def parse_data(str):
-    regex = re.compile(parser)
-    match = regex.match(str)
-    return (float(match[1]), float(match[2]))
-
+parser = r'\[\s*(\S*)\s+(\S*)\s*\]'
 project = "./mini-project/"
 train_20 = "noise_train_data(20_percent_noise).csv"
-x_train = []
-y_train = []
-with open(project + train_20) as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
-            line_count += 1
-        else:
-            line_count += 1
-            x_train.append(parse_data(row[0]))
-            y_train.append(row[1])
-            x1 = parse_data(row[0])[0]
-            x2 = parse_data(row[0])[1]
-            print('\tx = [' + str(x1) + ', ' + str(x2) + '];\ty = ' + str(row[1]))
 
-    print(f'Processed {line_count} lines.')
-x_train = np.asarray(x_train)
-y_train = np.asarray(y_train)
+def parse_data(data):
+    match = re.match(parser, data)
+    return (float(match[1]), float(match[2]))
+
+def parse_file(path):
+    x_array = []
+    y_array = []
+    with open(path) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        next(csv_reader)
+        for row in csv_reader:
+            data = parse_data(row[0])
+            x_array.append(data)
+            y_array.append(row[1])
+    return (np.asarray(x_array), np.asarray(y_array))
+
+x_train, y_train = parse_file(project + train_20)
+print("x_train and y_train generated successfully")
