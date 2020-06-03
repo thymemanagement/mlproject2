b8ddcc0b3badec7a695eeb2bfe9ec13525d19c78
Jordan Leggett <jleggett@ucsc.edu>
2020-06-02 19:35:38 -0700

    Limit re-labeling to ~20%

--- image_classification.py
+++ image_classification.py
@@ -1,5 +1,6 @@
 from preprocess import *
   
+import math
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
@@ -21,7 +22,7 @@ from numpy import asarray
 from numpy import save
 from classification_models.keras import Classifiers
 
-
+# a is the given label, b is predicted
 def test_labels_equal(a, b):
     max_index_a = 0
     max_index_b = 0
@@ -33,8 +34,8 @@ def test_labels_equal(a, b):
     if max_index_a == max_index_b:
         return 0.0
     else:
-        if b[max_index_b] > 0.80:
-            return 1.0
+        if b[max_index_b] > 0.60:
+            return 1.0 - b[max_index_a]
         else:
             return 0.0
 
@@ -52,6 +53,89 @@ def relabel(predicted_correct):
             ret.append(0.0)
     return np.asarray(ret)
 
+
+# If a labels score is predicted correct, but we want to try changing it anyway,
+# this function will return a random "next best guess" of sorts
+def relabel_random(predicted_correct):
+    scores = []
+    max_index = 0
+    for i in range(10):
+        if predicted_correct[i] > predicted_correct[max_index]:
+            max_index = i
+        scores.append((predicted_correct[i], i))
+
+    # Find a random index to try, ideally from the top few best
+    relabel_idx = 0
+    while relabel_idx == max_index: # Don't guess the same label were already given!
+        (score, idx) = scores[random.randrange(0, 3)]
+        relabel_idx = idx
+
+    ret = []
+    for i in range(10):
+        if i == relabel_idx:
+            ret.append(1.0)
+        else:
+            ret.append(0.0)
+    return np.asarray(ret)
+
+def relabel_round(given_labels, pictures, best_model, iters_since_last_improvement):
+     # don't trust prediction entirely, gives some randomness
+    if iters_since_last_improvement < 3:
+        salt_ratio = 0.00
+    elif iters_since_last_improvement < 10:
+        salt_ratio = 0.05
+    elif iters_since_last_improvement < 20:
+        salt_ratio = 0.25
+    elif iters_since_last_improvement < 30:
+        salt_ratio = 0.5
+    else:
+        salt_ratio = 0.75
+
+    out_labels = given_labels.copy()
+    n_labels = len(out_labels)
+    n_incorrect = int(n_labels * 0.2) # ~20% of labels are incorrect?
+
+    bad_label_indices = [] # (how_rong_given_label_is, index)
+    for i in range(n_labels):
+        c_img = pictures[i]
+        c_label = given_labels[i]
+        predicted_label = best_model.predict(np.asarray([c_img]))[0]
+        score = test_labels_equal(c_label, predicted_label)
+        if score != 0.0:
+            bad_label_indices.append((score, i))
+    bad_label_indices.sort()
+    bad_label_indices.reverse()
+
+    # How many of the bad_labels will we relabel?
+    if len(bad_label_indices) > n_incorrect:
+        n_to_relabel = int(math.floor(n_incorrect * (1.0 - salt_ratio)))
+    else:
+        n_to_relabel = int(len(bad_label_indices))
+
+    # How many random "so called correct" labels will we relabel?
+    n_to_salt = n_incorrect - n_to_relabel
+
+    print("Num relabel = " + str(n_to_relabel))
+    print("Num salt = " + str(n_to_salt))
+
+    # relabel the bad scored data
+    for i in range(n_to_relabel):
+        (score, relabel_idx) = bad_label_indices[i]
+        out_labels[relabel_idx] = relabel(out_labels[relabel_idx])
+
+    # re-label some random labels
+    while n_to_salt > 0:
+        rand_idx = random.randrange(0, n_labels)
+        # Check if we already relabeld rand_idx
+        index_alrealy_relabeld = False
+        for i in range(n_to_relabel):
+            (score, idx) = bad_label_indices[i]
+            if rand_idx == idx:
+                index_alrealy_relabeld = True
+                break
+        if not index_alrealy_relabeld:
+            out_labels[rand_idx] = relabel_random(out_labels[rand_idx])
+            n_to_salt -= 1
 
 def main():
     train_images_path = './train_images.npy'
@@ -93,7 +177,7 @@ def main():
     NUM_FILTERS = 32
     FILTER_SIZE = 3
     POOL_SIZE = 2
-    EPOCHS = 2
+    EPOCHS = 15
     BATCH_SIZE = 64 #64 or 128
     CONSTANT = 64 
 
@@ -114,22 +198,19 @@ def main():
 
     model.summary()
 
-    model_save_path = "best_model"
-    best_x = x_train
-    best_y = y_train
-    current_x = best_x.copy()
-    current_y = best_y.copy()
+    current_y = y_train.copy()
     best_accuracy = 0.0
+    iters_since_last_improvement = 0
     while True:
         sgd = optimizers.SGD(lr=0.001, momentum=0.9)
         model.compile(loss='categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
         history = model.fit(
-            current_x,
+            x_train,
             current_y,
             epochs=EPOCHS, 
             batch_size=BATCH_SIZE, 
             verbose=2,
-            validation_split=0.2)
+            validation_data=(x_test, y_test))
         current_accuracy = history.history['val_accuracy'][-1]
         print("Current accuracy = " + str(current_accuracy))
 
@@ -137,50 +218,23 @@ def main():
         if current_accuracy > best_accuracy:
             print("New best accuracy: " + str(current_accuracy) + " VS " + str(best_accuracy))
             best_accuracy = current_accuracy
-            best_x = current_x.copy()
-            best_y = current_y.copy()
-            model.save(model_save_path)
+            iters_since_last_improvement += 1
         else:
-            current_x = best_x.copy()
-            current_y = best_y.copy()
+            current_y = y_train.copy()
+            iters_since_last_improvement = 0
             # Have to re-build the old model since it was better.
             model.compile(loss='categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
             history = model.fit(
-                current_x,
+                x_train,
                 current_y,
                 epochs=EPOCHS, 
                 batch_size=BATCH_SIZE, 
                 verbose=2,
-                validation_split=0.2)
-            
+                validation_data=(x_test, y_test))
+        
+        # Re-label step
+        relabel_round(current_y, x_train, model, iters_since_last_improvement)
 
-        # go through best training data, and re-label anything bad
-        bad_training_indices = []
-        num_printed = 0
-        for i in range(len(current_x)):
-            c_img = current_x[i]
-            c_label = current_y[i]
-            predicted_label = model.predict(np.asarray([c_img]))[0]
-            predicted_str = label_encoder.inverse_transform([argmax([predicted_label])])
-            given_str = label_encoder.inverse_transform([argmax([c_label])])
-            if test_labels_equal(c_label, predicted_label) != 0.0:
-                if num_printed < 5:
-                    cv2.imshow('given ' + str(given_str) + ", predicted " + str(predicted_str),c_img)
-                    cv2.waitKey(0)
-                    num_printed += 1
-                bad_training_indices.append((test_labels_equal(c_label, predicted_label), i))
-
-        # From the list of bad training data, select a random set of 20 items to re-label.
-        num_bad_items = len(bad_training_indices)
-        bad_training_indices.sort()
-        print("Num bad training items: " + str(num_bad_items))
-        bad_training_indices = bad_training_indices[:20]
-
-        # Re-train the selected data
-        for (err, i) in bad_training_indices:
-            c_img = current_x[i]
-            predicted_label = model.predict(np.asarray([c_img]))[0]
-            current_y[i] = relabel(predicted_label)
 
 
 
